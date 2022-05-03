0812ed055f0b40358b2ceeb97c094e236c5e944d
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2022-05-03 20:13:05 +0300

    Implementing Repository pattern with Unit of work

---
+++ Infrastructure/Common/IRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Common
+{
+    public interface IRepository : IDisposable
+    {
+        IQueryable<T> All<T>() where T : class;
+
+        IQueryable<T> All<T>(Expression<Func<T, bool>> expression) where T : class;
+
+        IQueryable<T> AllReadonly<T>() where T : class;
+
+        IQueryable<T> AllReadonly<T>(Expression<Func<T, bool>> expression) where T : class;
+
+        Task<T> GetByIdAsync<T>(object id) where T : class;
+
+        Task<T> GetByIdsAsync<T>(object[] id) where T : class;
+
+        Task AddAsync<T>(T entity) where T : class;
+
+        Task AddRangeAsync<T>(IEnumerable<T> entities) where T : class;
+
+        void Update<T>(T entity) where T : class;
+
+        void UpdateRange<T>(IEnumerable<T> entities) where T : class;
+
+        Task DeleteAsync<T>(object id) where T : class;
+
+        void Delete<T>(T entity) where T : class;
+
+        void DeleteRange<T>(Expression<Func<T, bool>> deleteWhereClause) where T : class;
+
+        void Detach<T>(T entity) where T : class;
+
+        int SaveChanges();
+
+        Task<int> SaveChangesAsync();
+    }
+}
