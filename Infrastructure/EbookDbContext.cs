d3dfc4e5ea1ca6bbf4e9d1182b0d9a776bdd59f1
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2022-05-03 20:04:43 +0300

    Database successfully created.

---
+++ Infrastructure/EbookDbContext.cs
@@ -0,0 +1,49 @@
+﻿using Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class EbookDbContext : DbContext
+    {
+        public EbookDbContext()
+        {
+
+        }
+
+        public EbookDbContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Genre> Genres { get; set; }
+
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Author> Authors { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Review> Reviews { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            if (!builder.IsConfigured)
+            {
+                builder.UseSqlServer(DbConfiguration.ConnectionString);
+            }
+
+            base.OnConfiguring(builder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
