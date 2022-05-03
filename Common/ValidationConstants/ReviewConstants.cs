1f4c03cff5e779841c6c76b071b95337ee420ac2
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2022-05-03 19:33:31 +0300

    Created initial database models.

---
+++ Common/ValidationConstants/ReviewConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.ValidationConstants
+{
+    public class ReviewConstants
+    {
+        public const int VALUE_MIN = 0;
+        public const int VALUE_MAX = 10;
+
+        public const int COMMENT_MAXLENGTH = 6000;
+    }
+}
---
+++ Infrastructure/Models/User.cs
@@ -0,0 +1,44 @@
+﻿using Common.ValidationConstants;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models
+{
+    public class User
+    {
+        public User()
+        {
+            Id = Guid.NewGuid()
+                .ToString();
+
+            FavouriteBooks = new HashSet<Book>();
+            Reviews = new HashSet<Review>();
+        }
+
+        [Key]
+        public string Id { get; set; }
+
+        [Required]
+        [MaxLength(UserConstants.USERNAME_MAXLENGTH)]
+        public string Username { get; set; }
+
+        [Required]
+        [MaxLength(UserConstants.EMAIL_MAXLENGTH)]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public DateTime RegisterDate { get; set; }
+
+        public DateTime? SubscriptionDate { get; set; }
+
+        public virtual ICollection<Book> FavouriteBooks { get; set; }
+
+        public virtual ICollection<Review> Reviews { get; set; }
+    }
+}
