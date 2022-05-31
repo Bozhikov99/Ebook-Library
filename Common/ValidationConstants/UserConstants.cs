10c0964887961950633888d6edc2a19f132aa2e4
Heathen-Source <xarddet@gmail.com>
2022-05-31 18:13:40 +0300

    Implementation of Registration page.

--- Common/ValidationConstants/UserConstants.cs
+++ Common/ValidationConstants/UserConstants.cs
@@ -14,7 +14,7 @@ namespace Common.ValidationConstants
         public const int PASSWORD_MINLENGTH = 5;
         public const int PASSWORD_MAXLENGTH = 30;
 
-        public const int EMAIL_MINLENGTH = 15;
+        public const int EMAIL_MINLENGTH = 8;
         public const int EMAIL_MAXLENGTH = 100;
     }
 }
--- Core/ViewModels/User/RegisterUserModel.cs
+++ Core/ViewModels/User/RegisterUserModel.cs
@@ -1,11 +1,7 @@
 ﻿using Common.MessageConstants;
 using Common.ValidationConstants;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Core.ViewModels.User
 {
