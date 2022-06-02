f6ec28c4ebb6b6c900dc79384e31bf9c2a48afdf
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2022-06-02 16:32:10 +0300

    Added a genre multiselect to the book browsing page.

---
+++ Web/Views/Shared/Components/BrowseGenres/BrowseGenresViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Core.Services.Contracts;
+using Core.ViewModels.Genre;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Views.Shared.Components.BrowseGenres
+{
+    public class BrowseGenresViewComponent : ViewComponent
+    {
+        private readonly IGenreService genreService;
+
+        public BrowseGenresViewComponent(IGenreService genreService)
+        {
+            this.genreService = genreService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            IEnumerable<ListGenreModel> genres = await genreService.GetAllGenres();
+
+            return View(genres);
+        }
+    }
+}
