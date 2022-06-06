d8a56abf0b3620c7682ba17b6d774d8dbd7902f6
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2022-06-06 16:03:10 +0300

    Added the admin navbar and set the default page of the admin area as ManageUsers.cshtml

--- Core/ViewModels/User/ListUserModel.cs
+++ Core/ViewModels/User/ListUserModel.cs
@@ -4,7 +4,7 @@
     {
         public string Id { get; set; }
 
-        public string UseName { get; set; }
+        public string UserName { get; set; }
 
         public string Email { get; set; }
     }
