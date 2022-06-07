d9191218ad331c36ae42846b9f3f85d52284b109
Heathen-Source <xarddet@gmail.com>
2022-06-07 15:25:09 +0300

    Added styling for Creating and Editing books. Added a book create button. Adjusted review stars for further development in Details page. Removed Create and Edit in User directory.

---
+++ Web/Areas/Admin/Views/Shared/Components/AdminGenres/AdminGenresViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Core.Services.Contracts;
+using Core.ViewModels.Genre;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Areas.Admin.Views.Shared.Components.AdminGenresViewComponent
+{
+    public class AdminGenresViewComponent : ViewComponent
+    {
+        private IGenreService genreService;
+
+        public AdminGenresViewComponent(IGenreService genreService)
+        {
+            this.genreService = genreService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            IEnumerable<ListGenreModel> genres = await genreService.GetAllGenres();
+            return View(genres);
+        }
+
+    }
+}
