a9e064601cb4690488417113cdc8912bf670e287
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2022-06-07 21:11:58 +0300

    Adding rating star buttons.

---
+++ Web/wwwroot/js/review.js
@@ -0,0 +1,27 @@
+﻿let ratingDivElement = document.querySelector('#rating-div');
+let ratingStarElements = ratingDivElement.querySelectorAll('.bi');
+
+ratingStarElements.forEach(s => s.addEventListener('click', () => {
+    ratingStarElements.forEach(rs => {
+        rs.classList.remove('bi-star-fill');
+        rs.classList.add('bi-star');
+    });
+
+    let value = s.dataset.value;
+
+    for (var i = 0; i < value; i++) {
+        //bi-star-fill
+        let currentElement = ratingStarElements[i];
+        ToggleClass(currentElement);
+    }
+}))
+
+function ToggleClass(element) {
+    if (element.classList.contains('bi-star')) {
+        element.classList.remove('bi-star');
+        element.classList.add('bi-star-fill');
+    } else {
+        element.classList.remove('bi-star-fill');
+        element.classList.add('bi-star');
+    }
+}
