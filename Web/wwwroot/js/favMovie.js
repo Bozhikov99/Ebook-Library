fe76d5932d456d58aed990113ae0502745e803eb
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2022-06-07 11:23:37 +0300

    Successfully integrated the adding/removing books from favorites functionality with the Front-End.

--- Web/wwwroot/js/favMovie.js
+++ Web/wwwroot/js/favMovie.js
@@ -1,6 +1,6 @@
 ﻿let buttonElement = document.querySelector('#favButton');
-let id = document.querySelector('#book-id')
-    .textContent;
+//let id = document.querySelector('#book-id')
+//    .textContent;
 
 buttonElement.addEventListener('click', (e) => {
     let targetElement = e.target;
@@ -8,13 +8,9 @@ buttonElement.addEventListener('click', (e) => {
 });
 
 function ToggleClass(targetElement) {
-    if (targetElement.classList.contains('btn-warning')) {
-        targetElement.classList.remove('btn-warning');
-        targetElement.classList.add('btn-danger');
-        targetElement.textContent = 'Unfav';
+    if (targetElement.textContent=='Favorite') {
+        targetElement.textContent = 'Unfavorite';
     } else {
-        targetElement.classList.remove('btn-danger');
-        targetElement.classList.add('btn-warning');
-        targetElement.textContent = 'Fav';
+        targetElement.textContent = 'Favorite';
     }
 }
