826417e0eddaecc6bf1dac0ff2f15afaaea2d269
Heathen-Source <xarddet@gmail.com>
2022-06-08 19:12:26 +0300

    Added Read Book Button.

--- Infrastructure/EbookDbContext.cs
+++ Infrastructure/EbookDbContext.cs
@@ -33,7 +33,7 @@ namespace Infrastructure
         {
             if (!builder.IsConfigured)
             {
-                builder.UseSqlServer(DbConfiguration.ConnectionString);
+                builder.UseSqlServer(DbConfiguration.ContributorConnectionString);
             }
 
             base.OnConfiguring(builder);
